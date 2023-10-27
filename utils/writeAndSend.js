5dc84937d909b7d818d99fde1e016830d96d9f85
victoriajquinto <victoriajquinto@gmail.com>
2023-10-26 22:22:53 -0700

    stub emailer helper function

--- utils/writeAndSend.js
+++ utils/writeAndSend.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 let apiKey = process.env.GPTAPI;
 
 //make call to chat gpt. need name, mbti, ONE interest
-const content = async() => {
+const advice = async() => {
 
 };
+
+//TO DO: stub emailer function. needs topic, name, email, advice. see documentation for nodemailer
