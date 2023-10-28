85e8b8341cec66d1dad42b15e8375cfb21dc7710
victoriajquinto <victoriajquinto@gmail.com>
2023-10-27 21:08:22 -0700

    add cross-env dev dependency, remove depecrated file paths

--- src/db.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const dotenv = require('dotenv');
-const pgPromise = require('pg-promise');
-
-const pgp = pgPromise({});
-const pool = pgp(process.env.DATABASE_URL);
-
-module.exports = pool;
--- utils/axios.js
+++ utils/axios.js
@@ -12,15 +12,63 @@ AND send server req to PATCH /increment_email
 
 */
 
+const baseURL = 'https://9c96-107-138-175-211.ngrok-free.app';
+
+
 //axios request to get /user/:freq. needs freq
 const getUsers = async (freq) => {
+  const endpoint = `/user`; //may not accept template literal =/
+
+  return axios.get(baseURL + endpoint, {
+    params: {
+      freq
+    }
+  })
+  .then(res => {
+    console.log(res.data);
+    return res.data;
+  })
+  .catch(error => {
+    console.log("Error in getUsers axios call: ", error);
+  });
 
 };
 
 //axios request to post /content. needs topic, advice, user_id
-const postContent = async (topic, advice, user_id) => {};
+const postContent = async (topic, advice, user_id) => {
+  const endpoint = `/content`;
+
+  return axios.post(baseURL + endpoint, {
+    topic,
+    advice,
+    user_id
+  })
+  .then(res => {
+    console.log(res.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+};
 
 //axios request to patch /increment_email. needs user_id, emails_sent_count
 const patchIncrementEmail = async (user_id, emails_sent_count) => {
+  const endpoint = `/increment_email`;
 
+  return axios.patch(baseURL + endpoint, {
+    user_id,
+    emails_sent_count,
+  })
+  .then(res => {
+    console.log(res.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 };
+
+module.exports = {
+  getUsers,
+  postContent,
+  patchIncrementEmail
+}
