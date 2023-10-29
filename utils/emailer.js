37d1fb3b467bd09a238ee519ab492992ce7457db
victoriajquinto <victoriajquinto@gmail.com>
2023-10-29 11:01:04 -0700

    add new function to send pooled emails via nodemailer

--- utils/emailer.js
+++ utils/emailer.js
@@ -3,34 +3,77 @@ require('dotenv').config();
 // console.log(process.env);
 const email_password = process.env.EMAIL_PASSWORD;
 
+//for sending one email
 const transporter = nodemailer.createTransport({
-  service: "yahoo",
+  service: "iCloud",
   secure: false,
   auth: {
-    user: 'unlockepiphany',
+    user: 'unlockepiphany@icloud.com',
     pass: email_password
   }
-})
+});
 
-const sendEmail = async (userEmail, topic, advice) => {
+const sendFirstEmail = async (userEmail, topic, advice) => {
   const message = {
-    from: 'unlockepiphany@yahoo.com',
+    from: 'unlockepiphany@icloud.com',
     to: userEmail,
     subject: `Unlock Your Epiphany in ${topic}`,
     text: advice,
     html: `<p>${advice}<p>`
   }
 
-  transporter.sendMail(message, (error, info) => {
+  await transporter.sendMail(message, (error, info) => {
     if (error) {
       console.log('Error sending email: ', error);
     } else {
       console.log('Email sent: ', info.response);
+
+      //call post /content
+      //call patch /increment_email
     }
+  });
+
+
+};
+
+//for sending bulk emails:
+const bulkTransporter = nodemailer.createTransport({
+  service: "iCloud",
+  secure: false,
+  auth: {
+    user: 'unlockepiphany@icloud.com',
+    pass: email_password
+  }
+  pool: true,
+  maxConnections: 5
+});
+
+
+const sendBulkEmails = async (arrayOfEpiphanies) => {
+  arrayOfEpiphanies.forEach((epiphany) => {
+    const message = {
+      from: 'unlockepiphany@icloud.com',
+      to: epiphany.email,
+      subject: `Unlock Your Epiphany in ${epiphany.topic}`,
+      text: epiphany.advice,
+    }
+
+    await bulkTransporter.sendMail(message, (error, info) => {
+      if (error) {
+        console.error(`Error sending email to ${epiphany.email}:`, error);
+      } else {
+        console.log(`Email sent to ${epiphany.email}:`, info.response);
+
+        //call post /content
+        //call patch /increment_email
+      }
+    });
   });
 };
 
-// sendEmail('victoriajquinto@gmail.com', 'testing nodemailer', 'you\'re on your own, goodluck');
+sendEmail('victoriajquinto@gmail.com', 'testing nodemailer 2', 'you\'re on your own, goodluck');
+
+/* VERIFY CONNECTION
 
 transporter.verify((error, success) => {
   if (error) {
@@ -40,6 +83,11 @@ transporter.verify((error, success) => {
   }
 });
 
-module.exports = {sendEmail};
+*/
+
+module.exports = {
+  sendFirstEmail,
+  sendBulkEmails
+};
 
 
