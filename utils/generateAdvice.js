3744ad89ade378992eefb115a00201dae7f7ef95
victoriajquinto <victoriajquinto@gmail.com>
2023-10-30 21:35:44 -0700

    uncomment call to chatgpt-api

--- utils/generateAdvice.js
+++ utils/generateAdvice.js
@@ -8,6 +8,7 @@ const openai = new OpenAI({
 });
 
 
+
 //make call to chat gpt. need name, mbti, ONE interest
 const generateAdvice = async(name, mbti, topic) => {
   const completion = await openai.chat.completions.create({
@@ -18,14 +19,44 @@ const generateAdvice = async(name, mbti, topic) => {
   const advice = completion.choices[0];
   console.log(advice);
   return advice;
+
 };
 
-generateAdvice('Vicky', 'INFJ', 'Networking');
+// generateAdvice('Vicky', 'INFJ', 'Networking');
 
 module.exports = {
   generateAdvice
 }
 
+/*
+const mockAdvice = {
+  index: 0,
+  message: {
+    role: 'assistant',
+    content: `Dear ${name},\n' +
+      '\n' +
+      'As an ${mbti}, you tend to be introverted and value deep connections with others. ${topic} may seem daunting, but it can be a beneficial way to expand your horizons and establish meaningful professional relationships. Here are a few tips to help you navigate the world of ${topic}:\n' +
+      '\n' +
+      '1. Find events aligned with your interests: Seek out ${topic} events that cater to your passions and areas of expertise. Connecting with individuals who share common interests will make it easier for you to engage in meaningful conversations and build genuine connections.\n' +
+      '\n' +
+      '2. Take the time to prepare: Before attending an event or meeting, do your research. Learn about the individuals or organizations you want to connect with. This preparation will help you feel more confident and allow for more meaningful conversations when you meet them.\n' +
+      '\n' +
+      '3. Focus on quality over quantity: As an INFJ, you value depth and authenticity in your relationships, and this trait can shine through in networking situations. Focus on building genuine connections with a few individuals rather than trying to connect with a large number of people superficially. Deep connections can lead to more meaningful opportunities.\n' +
+      '\n' +
+      '4. Utilize your listening skills: One of your strengths as an ${mbti} is your ability to listen actively and empathetically. Use this skill to your advantage during networking events. Ask thoughtful questions, actively listen to the responses, and show genuine interest in the other person. This will help you establish deeper connections.\n' +
+      '\n' +
+      "5. Follow up: ${topic} doesn't end at the event. Take the initiative to follow up with the people you meet. Send a personalized email expressing your appreciation for the conversation and expressing your desire to stay connected. This will reinforce the connection and show that you value the relationship.\n" +
+      '\n' +
+      'Remember, ${topic} is not about self-promotion but rather about building authentic relationships. By staying true to your ${mbti} nature and focusing on quality connections, networking can become a powerful tool for professional growth and personal development.\n' +
+      '\n' +
+      'Best of luck on your ${topic} journey!\n' +
+      '\n' +
+      'Warm regards,\n' +
+      'Unlock Epiphany`
+  },
+  finish_reason: 'stop'
+}
+*/
 
 
 /*RETURN VALUE:
